12de493e8d5c9a4acc55f58c62cca7e59d680e61
ahasanov <ahasa@softserveinc.com>
2019-12-05 01:02:12 +0200

    create own template filter

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 from ..models import Post
+
+import markdown
 
 
 register = template.Library()
@@ -22,3 +25,6 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
 
 
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
