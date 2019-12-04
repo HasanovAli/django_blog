30af8ebbbd71582b1af9e5e6d9f5aa690f80032a
ahasa <ahasa@softserveinc.com>
2019-12-04 15:00:36 +0200

    add email sending

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
+from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForm
 from .models import Post
 
 
@@ -28,3 +30,22 @@ def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month,
                              publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{0} ({1}) recommends you reading "{2}"'.format(
+                cd.get('name'), cd.get('email'), post.title)
+            message = 'Read "{0}" at {1}\n\n{2}\'s comments: {3}'.format(
+                post.title, post_url, cd.get('name'), cd.get('comments'))
+            send_mail(subject, message, 'admin@myblog.com', [cd.get('to')])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
