423be9964c8f7947223c366c278542e93c441779
ahasa <ahasa@softserveinc.com>
2019-12-04 19:38:28 +0200

    add simple recommendation system connected to tags

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
@@ -48,10 +49,22 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # get all the tags' ids for current post
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    # get posts with one of the post_tags_ids tag
+    similar_posts = Post.published \
+        .filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts \
+        .annotate(same_tags=Count('tags')) \
+        .order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
                                                      'new_comment': new_comment,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form,
+                                                     'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
