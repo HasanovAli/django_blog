b5e440ae0b14a667cd6ff04159ad2438e3e2e21d
ahasa <ahasa@softserveinc.com>
2020-01-03 12:53:12 +0200

    -update psycopg2 -add ranking for searching -minor refactoring

--- blog/views.py
+++ blog/views.py
@@ -95,8 +95,10 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
+            # In this way, we set priority for different parts of post during searching for matches
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
             search_query = SearchQuery(query)
+            # SearchRank - class for result ranking
             results = Post.objects.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).\
-                filter(search=search_query).order_by('-rank')
+                filter(rank__gte=0.15).order_by('-rank')
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
